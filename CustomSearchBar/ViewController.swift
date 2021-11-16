c401ae2564166f6b8921d0b3b6a997fdaf0a156c
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2021-11-15 20:12:11 -0500

    Initial Commit

---
+++ CustomSearchBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomSearchBar
+//
+//  Created by Oscar David Myerston Vega on 15/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
